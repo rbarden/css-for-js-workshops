8dea643e7c9fcd2434c9863de9414172922424d6
Reid Andrew Barden <me@reidbarden.com>
2021-03-28 11:14:43 -0400

    mini-component-library :: select

--- mini-component-library/src/components/Select/Select.js
+++ mini-component-library/src/components/Select/Select.js
@@ -9,10 +9,61 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <IconWrapper style={{ '--size': '24px'}}>
+          <Icon id="chevron-down" strokeWidth={1} size={24} />
+        </IconWrapper>
+      </PresentationalBit>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`
+
+const PresentationalBit = styled.div`
+  background-color: red;
+  padding: 12px 16px;
+  padding-right: 52px;
+  background-color: ${COLORS.transparentGray15};
+  font-size: ${16/16}rem;
+  color: ${COLORS.gray700};
+  border-radius: 8px;
+  
+  ${NativeSelect}:focus + & {
+     outline: 1px dotted #212121;
+  }
+  
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  right: 10px;
+  bottom: 0;
+  margin: auto;
+  width: var(--size);
+  height: var(--size);
+  pointer-events: none;
+`
 
 export default Select;
